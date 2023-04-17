acf288c4f454145119949540effc74006d2cbb51
ednabarboza <ednabarbozalima@gmail.com>
2023-04-17 12:17:49 -0300

    finalizando o modulo 23 de js com dom

---
+++ M23_js_dom/exericioM03/exercicio1.js
@@ -0,0 +1,12 @@
+const listaItens = document.querySelectorAll(".item");
+console.log(listaItens.length);
+
+//Consolidando o seu conhecimento
+
+var lista = ["Laranja", "Vermelho", "Branco", "Amarelo", "Rosa"];
+console.log(lista);
+lista.splice(1, 1);
+console.log(lista);
+
+//Ao utilizar o método splice(), o primeiro parâmetro a ser informado é a posição do item a ser removido,
+//e o segundo parâmetro informa quantos itens serão removidos a partir de sua posição.
