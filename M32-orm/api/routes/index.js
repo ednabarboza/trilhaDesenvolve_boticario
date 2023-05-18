fbe02f8fcb7b5d79fcdfbe49fac90ba51a497844
ednabarboza <ednabarbozalima@gmail.com>
2023-05-18 14:10:48 -0300

    finalizando o modulo 32 de orm com node js - api com sequelize e mysql

---
+++ M32-orm/api/routes/index.js
@@ -0,0 +1,24 @@
+//routes/index.js
+
+const bodyParser = require("body-parser");
+
+const pessoas = require("./pessoasRoute");
+//adicionamos as rotas de niveis e turmas
+const niveis = require("./niveisRoute");
+const turmas = require("./turmasRoute");
+
+//adicionamos as instâncias de níveis e turmas
+//e refatoramos um pouco a função
+module.exports = (app) => {
+  app.use(bodyParser.json(), pessoas, niveis, turmas);
+};
+
+// const bodyParser = require("body-parser");
+// const pessoas = require("./pessoasRoute");
+
+// module.exports = (app) => {
+//   app.use(bodyParser.json());
+//   app.use(pessoas);
+// };
+
+//gerencia os arquivos de rotas
