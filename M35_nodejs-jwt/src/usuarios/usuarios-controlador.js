dfff4405baaf899179d240efa234993e285d6d7a
ednabarboza <ednabarbozalima@gmail.com>
2023-05-28 15:29:35 -0300

    finalizando o modulo 35

--- M35_nodejs-jwt/src/usuarios/usuarios-controlador.js
+++ M35_nodejs-jwt/src/usuarios/usuarios-controlador.js
@@ -1,6 +1,19 @@
 const Usuario = require("./usuarios-modelo");
 const { InvalidArgumentError, InternalServerError } = require("../erros");
 
+const jwt = require("jsonwebtoken");
+
+function criaTokenJWT(usuario) {
+  const payload = {
+    id: usuario.id,
+  };
+
+  const token = jwt.sign(payload, process.env.CHAVE_JWT, {
+    expiresIn: "15min",
+  });
+  return token;
+}
+
 module.exports = {
   adiciona: async (req, res) => {
     const { nome, email, senha } = req.body;
@@ -25,6 +38,12 @@ module.exports = {
         res.status(500).json({ erro: erro.message });
       }
     }
+  },
+
+  login: (req, res) => {
+    const token = criaTokenJWT(req.user);
+    res.set("Authorization", token);
+    res.status(204).send();
   },
 
   lista: async (req, res) => {
