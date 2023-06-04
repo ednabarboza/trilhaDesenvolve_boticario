81793b57a0eef9ff8a84ef595742a8b8dc9a6f0d
ednabarboza <ednabarbozalima@gmail.com>
2023-06-04 10:14:03 -0300

    feat: finalizando o modulo 37 de typescript parte 2

---
+++ M37_typescript-2/dist/js/enums/dias-da-semana.js
@@ -0,0 +1,10 @@
+export var DiasDaSemana;
+(function (DiasDaSemana) {
+    DiasDaSemana[DiasDaSemana["DOMINGO"] = 0] = "DOMINGO";
+    DiasDaSemana[DiasDaSemana["SEGUNDA"] = 1] = "SEGUNDA";
+    DiasDaSemana[DiasDaSemana["TERCA"] = 2] = "TERCA";
+    DiasDaSemana[DiasDaSemana["QUARTA"] = 3] = "QUARTA";
+    DiasDaSemana[DiasDaSemana["QUINTAM"] = 4] = "QUINTAM";
+    DiasDaSemana[DiasDaSemana["SEXTA"] = 5] = "SEXTA";
+    DiasDaSemana[DiasDaSemana["SABADO"] = 6] = "SABADO";
+})(DiasDaSemana || (DiasDaSemana = {}));
