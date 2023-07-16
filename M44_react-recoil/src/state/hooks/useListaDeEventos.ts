f745fcfa77e4e722dbe07756ac7ce2bd0f87efe8
Edna Barboza <83794408+ednabarboza@users.noreply.github.com>
2023-07-16 15:33:27 -0300

    Delete M44_react-recoil directory

--- M44_react-recoil/src/state/hooks/useListaDeEventos.ts
+++
@@ -1,9 +0,0 @@
-import { useRecoilValue } from "recoil";
-import { listaDeEventosState } from "../atom";
-import { eventosFiltradosState } from "../seletores";
-
-const useListaDeEventos = () => {
-  return useRecoilValue(eventosFiltradosState);
-};
-
-export default useListaDeEventos;
